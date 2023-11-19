d5d7159c9818b27f5658a596f9c689836cf606f8
Ravelos <86490406+Ravelos@users.noreply.github.com>
2023-11-19 20:16:04 -0300

    Update toti_exercises.py

--- toti_exercises.py
+++ toti_exercises.py
@@ -1,1 +1,11 @@
-Faça um programa em Python que peça ao usuário três números inteiros. Seu programa deverá passar para uma função os três números (na ordem que foram inseridos pelo usuário) e, então, a função deve imprimir o resultado da soma entre o segundo número e o resultado da multiplicação entre o primeiro e último número inseridos. Use como entrada os valores (nessa ordem): 177, 351 e 95.
+# Faça um programa em Python que peça ao usuário três números inteiros.
+# Seu programa deverá passar para uma função os três números (na ordem que foram inseridos pelo usuário) e, então,
+# a função deve imprimir o resultado da soma entre o segundo número e o resultado da multiplicação entre o primeiro e último número inseridos. Use como entrada os valores (nessa ordem): 177, 351 e 95.
+
+def operacao_matematica():
+  num1 = int(input("Digite o primeiro número: "))
+  num2 = int(input("Digite o segundo número: "))
+  num3 = int(input("Digite o segundo número: "))
+  return (num1 * num3) + num2
+
+operacao_matematica()
